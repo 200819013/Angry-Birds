c20cc2b3e7aa893e501c5341fb06342e2dd4b43a
200819013 <74884112+200819013@users.noreply.github.com>
2021-02-18 21:41:29 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,97 @@
+//These are the four main in built classes for our physics engine
+//conatants are for shortening the names of our classes
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+//We are creating an object called engine in the world class
+//engine is the world in this game
+var engine, world;
+//these are our bodies objects
+var box1, pig1;
+var backgroundImg,platform;
+var bird, slingShot;
+
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    //The object that we declared is of Engine class
+    engine = Engine.create();
+    //We have to attach a world to our engine
+    //The world is like the world and the engine contains all the plysics properties
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+    //PI stands for 180 degrees and determines the angle of log
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(100,100);
+
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
+}
+
+function draw(){
+    background(backgroundImg);
+    //Update position of object so they change/ engine is updated every time they move
+    Engine.update(engine);
+    //Adds a border thickness between two objects
+    strokeWeight(4);
+    //border is given by stroke function
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig3.display();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
+    platform.display();
+    //log6.display();
+    slingshot.display();    
+}
+
+function mouseDragged(){
+    //When you are dragging your mouse, the x and y position of your bird is being set with your mouse
+    //Inside the setPosition func, we have to choose which bodies we are going to change and how we change it, like with x and y position
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+    
+    slingshot.fly();
+}
+function keyPressed(){
+    if(keyCode===32){
+       slingshot.attach(bird.body);
+    }
+}
